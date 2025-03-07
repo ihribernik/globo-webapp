ca1ae4b4a253b80664d1995979acf09e4becde4c
Ivan Hribernik <cihribernik@gmail.com>
2025-03-07 10:41:45 -0300

    added terraform secret manager

--- variables.tf
+++ variables.tf
@@ -32,9 +32,13 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "api_key" {
+variable "api_key_secret_id" {
   type        = string
-  description = "(Required) API key for web app to talk to SaaS platform."
+  description = "(Required) Secret id for the API key for web app to talk to SaaS platform."
+}
+variable "ec2_role_name" {
+  type        = string
+  description = "(Required) Name of role to associate with EC2 instance profile."
 }
 
 variable "tfe_organization" {
